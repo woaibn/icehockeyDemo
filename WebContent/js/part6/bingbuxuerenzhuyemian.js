f6c60976874427b4fc9b4d8e9ed862bda85f5e04
25832 <25832@DESKTOP-Q8KVNF3.mshome.net>
2017-09-22 19:39:34 +0800

    lin2

---
+++ WebContent/js/part6/bingbuxuerenzhuyemian.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+	$(".saishixuanze").click(function(){
+		window.location.href = "../../views/part5/binglinchengxiazhuyemian.jsp"
+	});
+	$(".bingmianxuandian").click(function(){
+		window.location.href = "../../views/part6/bingmianxuandian.jsp"
+	});
+	$(".menjiangjishutongji").click(function(){
+		window.location.href = "../../views/part6/menjiangjishutongji.jsp"
+	});
+	$(".qiuyuanjishutongji").click(function(){
+		window.location.href = "../../views/part6/qiuyuanjishutongji.jsp"
+	});
+});
